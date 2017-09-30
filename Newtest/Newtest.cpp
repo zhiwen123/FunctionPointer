5901d2a29a2adfe094849007af4939b9a93889c5
Zhiwen Zhu <zhuzhiwen87@gmail.com>
2017-09-30 00:24:54 -0400

    Add function pointer functions

--- Newtest/Newtest.cpp
+++ Newtest/Newtest.cpp
@@ -1,12 +1,35 @@
-// Newtest.cpp : Defines the entry point for the console application.
+// FunctionPointer.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+//the operation function calls a operator declared outside to realize the operation
+int operation(int a, int b, int(*oper)(int, int))
+{
+	return oper(a, b);
+}
 
 int main()
 {
-	int a = 0;
-    return 0;
+	int(*p)(int a, int b);
+	p = add;
+	int c = p(1, 2);
+	printf("%d\n", c);
+
+	int sum = operation(1, 2, add);
+	int mul = operation(1, 2, multiply);
+	printf("sum=%d\n", sum);
+	printf("mul=%d\n", mul);
+	return 0;
 }
 
